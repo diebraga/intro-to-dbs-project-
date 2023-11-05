ab676e20bd29007c8a6bdb8cb338414a04f7d79f
diebraga <diebraga0055@gmail.com>
2023-11-05 17:29:18 -0300

    added new tables

--- database/connect.js
+++ database/connect.js
@@ -46,6 +46,104 @@ db.serialize(() => {
       }
       console.log('Created the "users" table.');
     });
+
+    // Create 'customers' table
+    db.run(`
+      CREATE TABLE IF NOT EXISTS customers (
+        customer_id INTEGER PRIMARY KEY,
+        name VARCHAR(100),
+        address VARCHAR(255),
+        email VARCHAR(100) UNIQUE,
+        phone_number VARCHAR(15),
+        balance DECIMAL(10, 2),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )  
+    `, (err) => {
+      if (err) {
+        console.error('Error creating customers table', err.message);
+        return;
+      }
+      console.log('Created the "customers" table.');
+    });
+
+    // Create 'stock' table
+    db.run(`
+      CREATE TABLE IF NOT EXISTS stock (
+        stock_id INTEGER PRIMARY KEY,
+        description VARCHAR(255),
+        price DECIMAL(10, 2),
+        quantity_in_stock INTEGER,
+        last_ordered_date DATE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP
+      )
+    `, (err) => {
+      if (err) {
+        console.error('Error creating stock table', err.message);
+        return;
+      }
+      console.log('Created the "stock" table.');
+    });
+
+    // Create 'orders' table
+    db.run(`
+      CREATE TABLE IF NOT EXISTS orders (
+        order_id INTEGER PRIMARY KEY,
+        customer_id INTEGER,
+        user_id INTEGER,
+        status VARCHAR(20),
+        total DECIMAL(10, 2),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(customer_id) REFERENCES customers(customer_id),
+        FOREIGN KEY(user_id) REFERENCES users(id)
+      )
+   `, (err) => {
+     if (err) {
+       console.error('Error creating orders table', err.message);
+       return;
+     }
+     console.log('Created the "orders" table.');
+   });
+
+    // Create 'order_items' table
+    db.run(`
+      CREATE TABLE IF NOT EXISTS order_items (
+        order_item_id INTEGER PRIMARY KEY,
+        order_id INTEGER,
+        stock_id INTEGER,
+        quantity INTEGER,
+        unit_price DECIMAL(10, 2),
+        line_total DECIMAL(10, 2),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP,
+        FOREIGN KEY(order_id) REFERENCES orders(order_id),
+        FOREIGN KEY(stock_id) REFERENCES stock(stock_id)
+      )
+    `, (err) => {
+      if (err) {
+        console.error('Error creating order_items table', err.message);
+        return;
+      }
+      console.log('Created the "order_items" table.');
+    });
+
+    // Create 'deliveries' table
+    db.run(`
+      CREATE TABLE IF NOT EXISTS deliveries (
+        delivery_id INTEGER PRIMARY KEY,
+        order_id INTEGER,
+        delivery_date DATE,
+        delivered_at TIMESTAMP,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(order_id) REFERENCES orders(order_id)
+      )
+    `, (err) => {
+      if (err) {
+        console.error('Error creating deliveries table', err.message);
+        return;
+      }
+      console.log('Created the "deliveries" table.');
+    });
   });
 });
 
