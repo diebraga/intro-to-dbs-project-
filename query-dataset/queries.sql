374ca241ee670d7767e97a175757543a0ac576ba
Diego Braga <diebraga0055@gmail.com>
2023-11-27 15:34:27 +0000

    shiped

--- query-dataset/queries.sql
+++ query-dataset/queries.sql
@@ -1,2 +1,7 @@
 -- Set all users' annual leave allowance to 29
 UPDATE users SET annual_leave_allowance = 29;
+
+-- Update orders with IDs 1, 2, and 3 to 'Shipped' status
+UPDATE orders
+SET status = 'Shipped'
+WHERE order_id IN (1, 2, 3);
